f9989909963afac0a9b6584a6d6a3b2be930a070
unlocker1205 <19130215@st.hcmuaf.edu.vn>
2021-12-28 13:18:40 +0700

    JSP SERVLET

--- src/main/java/com/laptopsellingwebsite/jsp_servlet_laptopsellingwebsite/service/ProductService.java
+++ src/main/java/com/laptopsellingwebsite/jsp_servlet_laptopsellingwebsite/service/ProductService.java
@@ -24,6 +24,14 @@ public class ProductService {
         return ProductDao.getInstance().getAllProduct();
     }
 
+    public List<Product> getAllProduct(int limit, int page) {
+        return ProductDao.getInstance().getAllProduct(limit, page);
+    }
+
+    public int getTotalPage() {
+        return ProductDao.getInstance().getTotalPage();
+    }
+
     public List getAllProducer() {
         return ProductDao.getInstance().getAllProducer();
     }
@@ -84,5 +92,132 @@ public class ProductService {
     public List sortProductWithProducer(Map<String, String> map, String hangsx, String temp) {
         return ProductDao.getInstance().sortProductWithProducer(map, hangsx, temp);
     }
-
+    public int getTotalPageByProducer(String producer) {
+        return ProductDao.getInstance().getTotalPageByProducer(producer);
+    }
+        public List getProductManufacturer(String manufacturer, int limit, int page) {
+        return ProductDao.getInstance().getProductManufacturer(manufacturer, limit, page);
+    }
 }
+
+
+
+
+//package com.laptopsellingwebsite.jsp_servlet_laptopsellingwebsite.service;
+//
+//import com.laptopsellingwebsite.jsp_servlet_laptopsellingwebsite.beans.Product;
+//import com.laptopsellingwebsite.jsp_servlet_laptopsellingwebsite.dao.ProductDao;
+//
+//import java.util.List;
+//import java.util.Map;
+//
+//public class ProductService {
+//    private static ProductService instance;
+//
+//    private ProductService() {
+//
+//    }
+//
+//    public static ProductService getInstance() {
+//        if (instance == null) {
+//            instance = new ProductService();
+//        }
+//        return instance;
+//    }
+//
+//    public List<Product> getAllProduct() {
+//        return ProductDao.getInstance().getAllProduct();
+//    }
+//
+//    public List<Product> getAllProduct(int limit, int page) {
+//        return ProductDao.getInstance().getAllProduct(limit, page);
+//    }
+//
+//    public List<Product> getAllProduct(String temp, int limit, int page) {
+//        return ProductDao.getInstance().getAllProduct(temp, limit, page);
+//    }
+//
+//    public int getTotalPage() {
+//        return ProductDao.getInstance().getTotalPage();
+//    }
+//
+//    public List getAllProducer() {
+//        return ProductDao.getInstance().getAllProducer();
+//    }
+//
+////    public List getAllProduct(String temp) {
+////        return ProductDao.getInstance().getAllProduct(temp);
+////    }
+//
+//
+//    public List getTopProduct1(int num) {
+//        return ProductDao.getInstance().getTopProduct1(num);
+//    }
+//
+//    public List getTopProductBestSeller(int num) {
+//        return ProductDao.getInstance().getTopProductBestSeller(num);
+//    }
+//
+//    public List getProductManufacturer(String manufacturer) {
+//        return ProductDao.getInstance().getProductManufacturer(manufacturer);
+//    }
+//
+//    public List getProductManufacturer(String manufacturer, int limit, int page) {
+//        return ProductDao.getInstance().getProductManufacturer(manufacturer, limit, page);
+//    }
+//
+//    public int getTotalPageByProducer(String producer) {
+//        return ProductDao.getInstance().getTotalPageByProducer(producer);
+//    }
+//
+////    public List getProductManufacturer(String manufacturer, String temp) {
+////        return ProductDao.getInstance().getProductManufacturer(manufacturer, temp);
+////    }
+//
+//    public List getProductManufacturer(String manufacturer, String temp, int limit, int page) {
+//        return ProductDao.getInstance().getProductManufacturer(manufacturer, temp, limit, page);
+//    }
+//
+//    public List getProducerWithID(String producer) {
+//        return ProductDao.getInstance().getProducerWithID(producer);
+//    }
+//
+//    public List getProductWithID(String ID) {
+//        return ProductDao.getInstance().getProductWithID(ID);
+//    }
+//
+//    public List getProductWithProducer(String producer) {
+//        return ProductDao.getInstance().getProductWithProducer(producer);
+//    }
+//
+//    public List getTopProducer(int num) {
+//        return ProductDao.getInstance().getTopProducer(num);
+//    }
+//
+//    public List sortWithPrice(int highPrice, int lowPrice) {
+//        return ProductDao.getInstance().sortWithPrice(highPrice, lowPrice);
+//    }
+//
+//    public List sortPriceWithProducer(int highPrice, int lowPrice, String hangSX) {
+//        return ProductDao.getInstance().sortPriceWithProducer(highPrice, lowPrice, hangSX);
+//    }
+//
+//    public List sortProduct(Map<String, String> map, String temp, int limit, int page) {
+//        return ProductDao.getInstance().sortProduct(map, temp, limit, page);
+//    }
+//
+//    public List sortProductWithProducer(Map<String, String> map, String hangsx) {
+//        return ProductDao.getInstance().sortProductWithProducer(map, hangsx);
+//    }
+//
+//    public List sortProductWithProducer(Map<String, String> map, String hangsx, String temp, int limit, int page) {
+//        return ProductDao.getInstance().sortProductWithProducer(map, hangsx, temp, limit, page);
+//    }
+//    public int totalPageSort(Map<String, String> map) {
+//        return ProductDao.getInstance().totalPageSort(map);
+//    }
+//
+//    public int totalPageSortByProducer(Map<String, String> map, String producer) {
+//        return ProductDao.getInstance().totalPageSortByProducer(map, producer);
+//    }
+//}
