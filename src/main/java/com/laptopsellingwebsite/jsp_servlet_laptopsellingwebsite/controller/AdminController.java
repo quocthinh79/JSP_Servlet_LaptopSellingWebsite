ba6e13b896b47461ef5a23d733626442d45885b8
unlocker1205 <19130215@st.hcmuaf.edu.vn>
2022-07-19 20:13:15 +0700

    Sắp xong trang admin

--- src/main/java/com/laptopsellingwebsite/jsp_servlet_laptopsellingwebsite/controller/AdminController.java
+++ src/main/java/com/laptopsellingwebsite/jsp_servlet_laptopsellingwebsite/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.laptopsellingwebsite.jsp_servlet_laptopsellingwebsite.controller;
 
 import com.google.common.collect.Multimap;
 import com.laptopsellingwebsite.jsp_servlet_laptopsellingwebsite.service.ProductService;
+import com.laptopsellingwebsite.jsp_servlet_laptopsellingwebsite.service.UserServices;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,23 +11,58 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Random;
 
-@WebServlet(name = "ProductController", value = "/Admin")
+//@WebServlet(name = "ProductController", value = "/Admin")
+@WebServlet(urlPatterns = {"/Edit", "/Delete", "/Add", "/Admin"})
 public class AdminController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        Multimap<String, String> map = (Multimap<String, String>) session.getAttribute("map");
-        if (map != null){
-            map.clear();
+        String URL = request.getRequestURI().substring(request.getRequestURI().lastIndexOf("/"), request.getRequestURI().length());
+        switch (URL) {
+            case "/Admin":
+                HttpSession session = request.getSession();
+                Multimap<String, String> map = (Multimap<String, String>) session.getAttribute("map");
+                if (map != null) {
+                    map.clear();
+                }
+                int page = 1;
+                if (request.getParameter("pageTTLT") == null && request.getParameter("pageTTLT") == ""){
+                    page = 1;
+                }
+                if (request.getParameter("pageTTLT") != null && request.getParameter("pageTTLT") != ""){
+                    page = Integer.parseInt(request.getParameter("pageTTLT"));
+                }
+                request.setAttribute("pageTTLT", page);
+                int limit = 24;
+                request.setAttribute("allProduct", ProductService.getInstance().getAllProduct(limit, page));
+                int totalPage = ProductService.getInstance().getTotalPage();
+                int total = (int) Math.ceil((double) totalPage / (double) limit);
+                request.setAttribute("totalPageTTLT", total);
+//                request.setAttribute("products", ProductService.getInstance().getAllProduct());
+//                request.setAttribute("productsDell", ProductService.getInstance().getProductWithProducer("DELL"));
+                request.getRequestDispatcher("jsp/admin.jsp").forward(request, response);
+                break;
         }
-        request.setAttribute("products", ProductService.getInstance().getAllProduct());
-        request.setAttribute("productsDell", ProductService.getInstance().getProductWithProducer("DELL"));
-        request.getRequestDispatcher("jsp/admin.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
+        String data = request.getParameter("send");
+        String currentTable = request.getParameter("currentTable");
+        int sizeData = data.split(",").length;
+        System.out.println(sizeData);
+        String URL = request.getRequestURI().substring(request.getRequestURI().lastIndexOf("/"), request.getRequestURI().length());
+        switch (URL) {
+            case "/Edit":
+                System.out.println("Edit");
+                break;
+            case "/Delete":
+                System.out.println("Delete");
+                break;
+            case "/Add":
+                System.out.println("Add");
+                break;
+        }
     }
 }
--- src/main/java/com/laptopsellingwebsite/jsp_servlet_laptopsellingwebsite/filter/Encode.java
+++ src/main/java/com/laptopsellingwebsite/jsp_servlet_laptopsellingwebsite/filter/Encode.java
@@ -1,9 +1,20 @@
 package com.laptopsellingwebsite.jsp_servlet_laptopsellingwebsite.filter;
 
-import javax.servlet.*;
 import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 public class Encode implements Filter {
+    public Encode(){
+
+    }
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
